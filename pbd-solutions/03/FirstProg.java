55f710fa4efedddb333d070deb756a44b39c05b5
joshvoss1 <joshua.voss18@ycdsbk12.ca>
2018-02-21 09:49:45 -0500

    Add files via upload

---
+++ pbd-solutions/03/FirstProg.java
@@ -0,0 +1,6 @@
+public class FirstProg {
+    public static void main(String[] args) {
+        System.out.println("Josh Voss is Zee Boss!");
+        System.out.println("Why are you reading this?");
+        }
+}
---
+++ pbd-solutions/06/LetterToYourself.java
@@ -0,0 +1,17 @@
+public class LetterToYourself 
+{
+    public static void main(String[] args)
+    {
+System.out.println("+----------------------------------------+");
+System.out.println("|                                   #### |");
+System.out.println("|                                   #### |");
+System.out.println("|                                   #### |");
+System.out.println("|                                        |");
+System.out.println("|                                        |");
+System.out.println("|                    Some Guy            |");
+System.out.println("|                    123 Hohoho          |");
+System.out.println("|                    North Pole, Canada  |");
+System.out.println("|                                        |");
+System.out.println("+----------------------------------------+");
+}
+}
