3a847c3197b0bbb33e038f9ae1830e977d1d2b3c
joshvoss1 <joshua.voss18@ycdsbk12.ca>
2018-02-22 09:23:32 -0500

    Add files via upload

---
+++ 08/NumbersAndMath.java
@@ -0,0 +1,24 @@
+public class NumbersAndMath
+{
+    public static void main(String[] args){
+    //prints phrase about counting chicken
+    System.out.println("I will now count my chickens:");
+    // calculates number of hens and displays them
+    System.out.println("Hens " + (25 + 30/6));
+    // calculates number of roosters and displays them
+    System.out.println("Roosters " + (100 -25 * 3 % 4));
+    //prints phrase about counting chicken
+    System.out.println("Now I will count the eggs: ");
+    // calculates number of eggs and displays them
+    System.out.println(3.0 + 3.0 + 1.0 - 5.0 + 4.0 % 2.0 -1.0 / 4.0 + 6.0);
+    System.out.println("Is it true that 3+ 2 < 5 - 7 ?"); 
+    System.out.println(3.0 + 2.0 < 5.0 - 7.0);
+    System.out.println("What is 3 + 2? " + (3.0 + 2.0));
+    System.out.println("What is 5 - 7? " + (5.0 - 7.0));
+    System.out.println("Oh, that's why it is false.");
+    System.out.println("How about some more.");
+    System.out.println("Is it greater? " + (5.0 > -2.0));
+    System.out.println("Is it greater or equal? " + (5 >= -2.0));
+    System.out.println("Is it less or equal? " +(5.0 <= -2.0));
+    }
+    }
